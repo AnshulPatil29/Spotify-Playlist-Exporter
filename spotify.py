17872fde4cd047963f0ef2a566abe982ff826d51
AnshulPatil29 <Aaprocks29@gmail.com>
2025-05-14 20:36:38 +0530

    Added ID extraction feature

--- spotify.py
+++ spotify.py
@@ -17,6 +17,9 @@ except (FileNotFoundError, KeyError) as e:
 
 # define the access that the Oauth Provides from SpotifyAPI
 SCOPE = "user-library-read playlist-read-private playlist-read-collaborative"
+
+# setup authmanager for authorization
+# note that this does not call the API,this just sets it up for later
 auth_manager=SpotifyOAuth(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
@@ -24,3 +27,39 @@ auth_manager=SpotifyOAuth(
     scope=SCOPE
 ) 
 sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
+def id_helper_name(playlist_name:str,sp=sp)->str:
+    results = sp.current_user_playlists(limit=50)
+    while True:
+        if results['items']:
+            for item in results['items']:
+                if 'name' in item and 'id' in item:
+                    if item['name']==playlist_name:
+                        return item['id']
+        if results['next']:
+            results = sp.next(results)
+        else:
+            return None
+        
+def id_helper_url(playlist_url:str)->str:
+    return playlist_url[34:56]
+
+
+
+def get_playlist_id(playlist:str,sp=sp)->str:
+    '''
+    returns: playlist id or '' for liked songs
+    '''
+    # playlist id is 22 characters long, and starts right after 'https://open.spotify.com/playlist/' which is 34 characters long
+    if playlist.startswith('https://open.spotify.com/playlist'):
+        return id_helper_url(playlist)
+    
+    if playlist is None or playlist=='':
+        return ''
+    
+    result=id_helper_name(playlist,sp)
+    if result is None:
+        print('Playlist not found')
+        exit()
+    return result
