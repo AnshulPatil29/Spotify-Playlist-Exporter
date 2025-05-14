c0968b2c114dc1ac83790eb6aa03275f2db2b718
AnshulPatil29 <Aaprocks29@gmail.com>
2025-05-14 17:29:06 +0530

    setup config and auth manager

--- spotify.py
+++ spotify.py
@@ -1,9 +1,26 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
-import os
+import json
+
+# import client id, secret and URI from config.json
+try:
+    with open('config.json','r') as f:
+        spotify_data=json.load(f)['SPOTIFY']
+    CLIENT_ID=spotify_data['CLIENT_ID']
+    CLIENT_SECRET=spotify_data['CLIENT_SECRET']
+    REDIRECT_URI=spotify_data['REDIRECT_URI']
+except (FileNotFoundError, KeyError) as e:
+    print(f"Error loading configuration from config.json: {e}")
+    exit()
+    
 
 # define the access that the Oauth Provides from SpotifyAPI
-scopes = "user-library-read playlist-read-private playlist-read-collaborative" 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scopes))
-
+SCOPE = "user-library-read playlist-read-private playlist-read-collaborative"
+auth_manager=SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope=SCOPE
+) 
+sp = spotipy.Spotify(auth_manager=auth_manager)
